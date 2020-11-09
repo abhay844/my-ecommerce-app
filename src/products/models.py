5cd0dfd67d18a1e195f7bde8d2fef89f7201e22d
abhay844 <akatiyar28@gmail.com>
2020-11-09 21:34:47 +0530

    Registered model in admin panel.

--- src/products/models.py
+++ src/products/models.py
@@ -6,3 +6,6 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=15, default=39.99)
+
+    def __str__(self):
+        return self.title
